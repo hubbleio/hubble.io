5ab0fbc52a9fb73f93669fab09e4e8afb52d1181
Pedro Teixeira <pedro.teixeira@gmail.com>
2012-07-19 14:44:14 +0100

    [templating] restructured how templating is being done

--- server/templates/index.js
+++ server/templates/index.js
@@ -1,7 +1,18 @@
-module.exports = function(html, templates) {
+module.exports = function(html, templates, conf, bind, articles) {
+
+
+
   return function(html) {
-    return templates('/layout.html')(html, {
-      title: 'Home'
+    var data = {
+      'guides-popular': templates('/article/list')(popularGuides),
+      'guides-new': templates('/articles/')(newGuides)
+    };
+
+    var main = bind(data, html);
+    return templates('/layout.html')({
+      main: main,
+      title: 'Home',
+      orgname: conf.title
     });
   };
 };
--- server/templates/layout.js
+++ server/templates/layout.js
@@ -1,11 +1,20 @@
-module.exports = function(html, templates, conf, bind) {
-  return function(main, options) {
+module.exports = function(html, templates, conf, bind, Map) {
+  return function(options) {
+    var map = Map();
+    map.class('orgname').to('orgname');
+
     var data = {
       //menu: templates['categories.html'](categories),
-      main: main,
-      orgname: conf.orgname || 'Orgname',
-      title: 'Node Guides - ' + options.title
+      main: options.main,
+      title: conf.title + ' - ' + options.title,
+      orgname: conf.title,
+      tagline: conf.tagline,
+      description: conf.description,
+      beginner: conf.content.home.beginner,
+      intermediate: conf.content.home.beginner,
+      expert: conf.content.home.expert
     };
+
 
     return bind(html, data);
   };
