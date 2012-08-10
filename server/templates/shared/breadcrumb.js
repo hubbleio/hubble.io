031e4c046054bb29fa2fd01fcde47c9d507c1ff4
Pedro Teixeira <pedro.teixeira@gmail.com>
2012-08-10 16:43:31 +0100

    [ux] fix breadcrumb URL to category page

--- server/templates/shared/breadcrumb.js
+++ server/templates/shared/breadcrumb.js
@@ -5,9 +5,16 @@ module.exports = function(html, templates, conf, bind, Map, content) {
     if (! Array.isArray(parts)) {
       parts = [parts];
     }
+
+    var accumulatedPath = [];
     
     var data = {
       breadcrumb: parts.map(function(part, idx) {
+        accumulatedPath.push(part);
+        part = {
+          url: '/categories/' + accumulatedPath.join('--'),
+          label: part
+        };
         if (idx < parts.length - 1) {
           return templates('/shared/breadcrumb_part.html').call(this, part);
         } else {
