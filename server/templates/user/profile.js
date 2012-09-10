fb54e31b6c1d62dd5bca0e26201b98ccabc8ba0a
Pedro Teixeira <pedro.teixeira@gmail.com>
2012-09-10 16:12:08 +0100

    [ux session] logout button added to header. Fixes #40.

--- server/templates/user/profile.js
+++ server/templates/user/profile.js
@@ -11,14 +11,11 @@ module.exports = function(html, templates, conf, bind, Map, content) {
     //
     // clone the categories so we can reduce it
     //
-    var map = Map();
-    map['class']('username').to('username');
-
     var data = {
       username: makeSafe(this.req.session.user.login)
     };
 
-    return bind(html, data, map);
+    return bind(html, data);
   };
   
 };
